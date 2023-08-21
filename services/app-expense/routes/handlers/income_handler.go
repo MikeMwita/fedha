24c0b02198e5a6fe14afe0cfab19907b81321423
MikeMwita <mwitamike8@gmail.com>
2023-08-21 21:55:57 +0300

    implemented expense handler

--- services/app-expense/routes/handlers/income_handler.go
+++ services/app-expense/routes/handlers/income_handler.go
@@ -3,43 +3,19 @@ package handlers
 import (
 	"context"
 	"errors"
+	"fmt"
 	"github.com/MikeMwita/fedha-go-gen.grpc/sdk/go-proto-gen/db"
 	"github.com/MikeMwita/fedha-go-gen.grpc/sdk/go-proto-gen/expense"
 	"google.golang.org/grpc"
 )
 
-//func (h Handler) CreateTrade(ctx context.Context, request *exchange.CreateTradeRequest) (*exchange.CreateTradeResponse, error) {
-//	if request == nil {
-//		return nil, ErrEmptyRequest
-//	}
-//
-//	tradeRequest := &db.CreateTradeRequest{
-//		Trade: &db.Trade{
-//			AccountId:    request.Trade.AccountId,
-//			TradeType:    request.Trade.TradeType,
-//			TradeStatus:  request.Trade.TradeStatus,
-//			FromCurrency: request.Trade.FromCurrency,
-//			ToCurrency:   request.Trade.ToCurrency,
-//			FromAmount:   request.Trade.FromAmount,
-//		},
-//	}
-//
-//	tradeResponse, err := h.tradeService.CreateTrade(ctx, tradeRequest)
-//	if err != nil {
-//		return nil, err
-//	}
-//
-//	tradeResponseResult := &exchange.CreateTradeResponse{
-//		TradeId: tradeResponse.TradeId,
-//	}
-//
-//	return tradeResponseResult, nil
-//}
 var (
-	ErrEmptyRequest = errors.New("empty request")
+	ErrEmptyRequest       = errors.New("empty request")
 	ErrAmountLessThanZero = errors.New("amount less than zero")
+	ErrEmptyIncomeID      = errors.New("empty income ID")
+	ErrDeleteIncome       = errors.New("error deleting income")
 )
-(ctx context.Context, request *expense.CreateIncomeRequest) (*expense.CreateIncomeResponse, error)
+
 func (h *Handler) CreateIncome(ctx context.Context, request *expense.CreateIncomeRequest, opts ...grpc.CallOption) (*expense.CreateIncomeResponse, error) {
 	if request == nil {
 		return nil, ErrEmptyRequest
@@ -47,28 +23,74 @@ func (h *Handler) CreateIncome(ctx context.Context, request *expense.CreateIncom
 	if request.Amount <= 0 {
 		return nil, ErrAmountLessThanZero
 	}
-
 	incomeRequest := &db.CreateIncomeRequest{
 		Income: &db.Income{
 			Amount: request.Amount,
-			Date:   request.Date.AsTime(),
 		},
 	}
-	createdIncome,err := h.incomeService.CreateIncome(ctx, incomeRequest)
+	createdIncome, err := h.incomeService.CreateIncome(ctx, incomeRequest)
 	if err != nil {
 		return nil, err
 	}
-	return createdIncome,nil
+	response := &expense.CreateIncomeResponse{
+		IncomeId: createdIncome.GetIncomeId(),
+	}
+
+	return response, nil
 }
 
 func (h *Handler) GetIncome(ctx context.Context, in *expense.GetIncomeRequest, opts ...grpc.CallOption) (*expense.GetIncomeResponse, error) {
+	if in == nil {
+		return nil, ErrEmptyRequest
+	}
+	income, err := h.incomeService.GetIncome(ctx, &db.GetIncomeRequest{
+		IncomeId: in.GetIncomeId(),
+	})
+	if err != nil {
+		return nil, err
+	}
+	response := &expense.GetIncomeResponse{
+		Income: &expense.Income{
+			Amount: income.Amount,
+		},
+	}
 
+	return response, nil
 }
 
 func (h *Handler) UpdateIncome(ctx context.Context, in *expense.UpdateIncomeRequest, opts ...grpc.CallOption) (*expense.UpdateIncomeResponse, error) {
+	if in == nil {
+		return nil, ErrEmptyRequest
+	}
+	updateRequest := &db.UpdateIncomeRequest{
+		IncomeId: in.GetIncomeId(),
+		NewIncome: &db.Income{
+			Amount: in.GetAmount(),
+		},
+	}
+	updatedIncome, err := h.incomeService.UpdateIncome(ctx, updateRequest)
+	if err != nil {
+		return nil, err
+	}
+	response := &expense.UpdateIncomeResponse{
+		IncomeId: updatedIncome.GetIncomeId(),
+	}
 
+	return response, nil
 }
 
 func (h *Handler) DeleteIncome(ctx context.Context, in *expense.DeleteIncomeRequest, opts ...grpc.CallOption) (*expense.DeleteIncomeResponse, error) {
+	incomeID := in.GetIncomeId()
+	if incomeID == "" {
+		return nil, ErrEmptyIncomeID
+	}
+	deleteRequest := &db.DeleteIncomeRequest{IncomeId: incomeID}
 
+	err, _ := h.incomeService.DeleteIncome(ctx, deleteRequest)
+	if err != nil {
+		return nil, ErrDeleteIncome
+	}
+	return &expense.DeleteIncomeResponse{
+		Message: fmt.Sprintf("income %s deleted successfully", incomeID),
+	}, nil
 }
