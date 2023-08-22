f88420da88422f41153867a69bec852ff41667f9
MikeMwita <mwitamike8@gmail.com>
2023-08-22 12:28:09 +0300

    updated matching methods

--- services/app-auth/internal/core/adapters/repo.go
+++ services/app-auth/internal/core/adapters/repo.go
@@ -13,24 +13,16 @@ import (
 
 type AuthRepo interface {
 	Register(ctx context.Context, registerReq dto.RegisterRequest) (*dto.RegisterResponseData, error)
-	//Login(ctx context.Context, loginReq dto.LoginRequest) (*dto.LoginResponseData, error)
 	Login(ctx context.Context, loginReq dto.LoginRequest) (*dto.LoginResponseData, error)
-	//GetUserById(c *gin.Context, userId string) (string, error)
 	GetUserByID(ctx context.Context, in *db.GetUserByIDRequest, opts ...grpc.CallOption) (*db.RegUserRes, error)
-
 	RefreshToken(c *gin.Context, data dto.RefreshTokenRequest) (*dto.RefreshTokenResponse, error)
 	UpdateUser(ctx context.Context, user entity.User) (*entity.User, error)
 	UserLogout(c *gin.Context)
 	SetAccessToken(ctx context.Context, key string, value string, expiration time.Duration) error
-	//GetAccessToken(ctx context.Context, value string) error
 	GetAccessToken(ctx context.Context, value string) (string, error)
-
 	DeleteAccessToken(ctx context.Context, key string) error
 	FindByUsername(ctx context.Context, username string) (*db.RegUserRes, error)
 	SaveUser(ctx context.Context, in *db.SaveUserRequest, opts ...grpc.CallOption) (*db.User, error)
-
-	//FindByUsername(ctx context.Context, username string) error
-	//SaveUser(ctx context.Context, user entity.User) error
 	DeleteSession(ctx context.Context, record *service.SessionRecord) error
 	InvalidateSession() error
 	VerifyRefreshToken(token string) error
