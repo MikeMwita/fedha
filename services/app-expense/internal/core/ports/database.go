138997f9faab2a4f78091ad82c7a10f167e3c433
MikeMwita <mwitamike8@gmail.com>
2023-08-31 23:50:29 +0300

    app-expense db

--- services/app-expense/internal/core/ports/database.go
+++ services/app-expense/internal/core/ports/database.go
@@ -7,12 +7,9 @@ import (
 )
 
 type DbStorage interface {
-
-	//HealthCheck(ctx context.Context, in *db.DefaultRequest) (*db.HealthResponse, error)
-
-	CreateExpense(ctx context.Context, in *db.ExpenseRequest, opts ...grpc.CallOption) (*db.ExpenseResponse, error)
-	GetExpense(ctx context.Context, in *db.GetExpenseRequest, opts ...grpc.CallOption) (*db.ExpenseResponse, error)
-	UpdateExpense(ctx context.Context, in *db.UpdateExpenseRequest, opts ...grpc.CallOption) (*db.ExpenseResponse, error)
+	CreateExpense(ctx context.Context, in *db.CreateExpenseRequest, opts ...grpc.CallOption) (*db.CreateExpenseResponse, error)
+	GetExpense(ctx context.Context, in *db.GetExpenseRequest, opts ...grpc.CallOption) (*db.GetExpenseResponse, error)
+	UpdateExpense(ctx context.Context, in *db.UpdateExpenseRequest, opts ...grpc.CallOption) (*db.UpdateExpenseResponse, error)
 	DeleteExpense(ctx context.Context, in *db.DeleteExpenseRequest, opts ...grpc.CallOption) (*db.DeleteExpenseResponse, error)
 	// INCOMES
 	CreateIncome(ctx context.Context, in *db.CreateIncomeRequest, opts ...grpc.CallOption) (*db.CreateIncomeResponse, error)
@@ -26,8 +23,8 @@ type DbStorage interface {
 }
 
 type DbService interface {
-	CreateExpense(context.Context, *db.ExpenseRequest) (*db.ExpenseResponse, error)
-	GetExpense(context.Context, *db.GetExpenseRequest) (*db.ExpenseResponse, error)
-	UpdateExpense(context.Context, *db.UpdateExpenseRequest) (*db.ExpenseResponse, error)
-	DeleteExpense(context.Context, *db.DeleteExpenseRequest) (*db.ExpenseResponse, error)
+	CreateExpense(context.Context, *db.CreateExpenseRequest) (*db.CreateExpenseResponse, error)
+	GetExpense(context.Context, *db.GetExpenseRequest) (*db.GetExpenseResponse, error)
+	UpdateExpense(context.Context, *db.UpdateExpenseRequest) (*db.UpdateExpenseResponse, error)
+	DeleteExpense(context.Context, *db.DeleteExpenseRequest) (*db.DeleteExpenseResponse, error)
 }
