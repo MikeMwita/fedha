59e0ab5a82b9da96ed5f7f5cb131204fae803c14
MikeMwita <mwitamike8@gmail.com>
2023-09-20 10:07:09 +0300

    stk push integration

---
+++ services/app-payment/models/stk.go
@@ -0,0 +1,27 @@
+package models
+
+type STKPushRequestBody struct {
+	BusinessShortCode string `json:"BusinessShortCode"`
+	Password          string `json:"Password"`
+	Timestamp         string `json:"Timestamp"`
+	TransactionType   string `json:"TransactionType"`
+	Amount            string `json:"Amount"`
+	PartyA            string `json:"PartyA"`
+	PartyB            string `json:"PartyB"`
+	PhoneNumber       string `json:"PhoneNumber"`
+	CallBackURL       string `json:"CallBackURL"`
+	AccountReference  string `json:"AccountReference"`
+	TransactionDesc   string `json:"TransactionDesc"`
+	CallbackURL       string `json:"callbackUrl"`
+}
+
+type STKPushRequestResponse struct {
+	MerchantRequestID   string `json:"MerchantRequestID"`
+	CheckoutRequestID   string `json:"CheckoutRequestID"`
+	ResponseCode        string `json:"ResponseCode"`
+	ResponseDescription string `json:"ResponseDescription"`
+	CustomerMessage     string `json:"CustomerMessage"`
+	RequestID           string `json:"requestId"`
+	ErrorCode           string `json:"errorCode"`
+	ErrorMessage        string `json:"errorMessage"`
+}
