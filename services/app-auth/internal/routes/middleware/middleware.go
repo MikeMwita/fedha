fbf12d1a5d92f202819dfb0c9ed9b7775ebad7fc
MikeMwita <mwitamike8@gmail.com>
2023-10-10 12:54:20 +0300

    implement app-auth middleware

--- services/app-auth/internal/routes/middleware/middleware.go
+++ services/app-auth/internal/routes/middleware/middleware.go
@@ -3,16 +3,25 @@ package middleware
 import (
 	"github.com/MikeMwita/fedha.git/services/app-auth/config"
 	"github.com/MikeMwita/fedha.git/services/app-auth/internal/core/adapters"
+	"github.com/MikeMwita/fedha.git/services/app-auth/platform/Monitoring/Prometheus"
+	"github.com/gin-gonic/gin"
 )
 
-type MiddlewareManager struct {
-	Config      config.Config
-	AuthUsecase adapters.AuthUseCase
+type MiddlewareManager interface {
+	Auth(ctx *gin.Context)
+	Cors(ctx *gin.Context)
 }
 
-func NewManager(cfg config.Config, authUseCase adapters.AuthUseCase) *MiddlewareManager {
-	return &MiddlewareManager{
+type middlewareManager struct {
+	Config      config.Config
+	AuthUsecase adapters.AuthUseCase
+	promMetrics *Prometheus.Metrics
+}
+
+func NewMiddlewareManager(cfg config.Config, authUsecase adapters.AuthUseCase, promMetrics *Prometheus.Metrics) MiddlewareManager {
+	return &middlewareManager{
 		Config:      cfg,
-		AuthUsecase: authUseCase,
+		AuthUsecase: authUsecase,
+		promMetrics: promMetrics,
 	}
 }
